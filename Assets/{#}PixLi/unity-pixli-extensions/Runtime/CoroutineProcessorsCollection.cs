0145957c0793a5a09f81ea19d80ba1f9aed50b5f
Pixel Lifetime <maxymkarpinsky@gmail.com>
2021-07-18 21:03:07 +0300

    Add processors collections.

--- Assets/{#}PixLi/unity-pixli-extensions/Runtime/CoroutineProcessorsCollection.cs
+++ Assets/{#}PixLi/unity-pixli-extensions/Runtime/CoroutineProcessorsCollection.cs
@@ -44,6 +44,34 @@ public static class CoroutineProcessorsCollection
 	public static IEnumerator InvokeAfterRealtime(float seconds, Action action) => 
 		CoroutineProcessorsCollection.InvokeAfterInline(new WaitForSecondsRealtime(seconds), action);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal static IEnumerator InvokeIndefinitelyInline(YieldInstruction yieldInstruction, Action action)
+	{
+		while (true)
+		{
+			action.Invoke();
+
+			yield return yieldInstruction;
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal static IEnumerator InvokeIndefinitelyInline(CustomYieldInstruction customYieldInstruction, Action action)
+	{
+		while (true)
+		{
+			action.Invoke();
+
+			yield return customYieldInstruction;
+		}
+	}
+
+	public static IEnumerator InvokeIndefinitely(YieldInstruction yieldInstruction, Action action) =>
+		CoroutineProcessorsCollection.InvokeIndefinitelyInline(yieldInstruction, action);
+
+	public static IEnumerator InvokeIndefinitely(CustomYieldInstruction customYieldInstruction, Action action) =>
+		CoroutineProcessorsCollection.InvokeIndefinitelyInline(customYieldInstruction, action);
+
 #if UNITY_EDITOR
 #endif
 }
