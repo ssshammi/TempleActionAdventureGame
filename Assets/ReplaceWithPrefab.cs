08179a5942cd6c216ad9937be943b3d6b94c873f
Pixel Lifetime <maxymkarpinsky@gmail.com>
2021-07-22 05:08:33 +0300

    Replace floor tiles and most walls. Organize prefabs, textures, materials.

---
+++ Assets/ReplaceWithPrefab.cs
@@ -0,0 +1,60 @@
+using UnityEditor;
+using UnityEngine;
+
+public class ReplaceWithPrefab : EditorWindow
+{
+	[SerializeField] private GameObject prefab;
+
+	[MenuItem("Tools/Replace With Prefab")]
+	private static void CreateReplaceWithPrefab()
+	{
+		EditorWindow.GetWindow<ReplaceWithPrefab>();
+	}
+
+	private void OnGUI()
+	{
+		this.prefab = (GameObject)EditorGUILayout.ObjectField("Prefab", this.prefab, typeof(GameObject), false);
+
+		if (GUILayout.Button("Replace"))
+		{
+			var selection = Selection.gameObjects;
+
+			for (var i = selection.Length - 1; i >= 0; --i)
+			{
+				var selected = selection[i];
+
+				PrefabAssetType prefabAssetType = PrefabUtility.GetPrefabAssetType(this.prefab);
+				PrefabInstanceStatus prefabInstanceStatus = PrefabUtility.GetPrefabInstanceStatus(this.prefab);
+
+				GameObject newObject;
+
+				if (prefabAssetType != PrefabAssetType.NotAPrefab && prefabAssetType != PrefabAssetType.MissingAsset)
+				{
+					newObject = (GameObject)PrefabUtility.InstantiatePrefab(this.prefab);
+				}
+				else
+				{
+					newObject = Instantiate(this.prefab);
+					newObject.name = this.prefab.name;
+				}
+
+				if (newObject == null)
+				{
+					Debug.LogError("Error instantiating prefab");
+					break;
+				}
+
+				Undo.RegisterCreatedObjectUndo(newObject, "Replace With Prefabs");
+				newObject.transform.parent = selected.transform.parent;
+				newObject.transform.localPosition = selected.transform.localPosition;
+				newObject.transform.localRotation = selected.transform.localRotation;
+				newObject.transform.localScale = selected.transform.localScale;
+				newObject.transform.SetSiblingIndex(selected.transform.GetSiblingIndex());
+				Undo.DestroyObjectImmediate(selected);
+			}
+		}
+
+		GUI.enabled = false;
+		EditorGUILayout.LabelField("Selection count: " + Selection.objects.Length);
+	}
+}
